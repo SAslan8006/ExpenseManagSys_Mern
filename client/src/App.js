0b0d8573260e72e20c28cfaf0dc2eea6f3a491a4
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-04-11 00:12:13 +0300

    antd fixed

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import "antd/dist/reset.css";
 
 function App() {
   return (
