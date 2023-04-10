905900bdcff8992b2310ffb4a142e50eda40f629
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-04-10 23:30:07 +0300

    Login and Register created

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 const express = require("express");
-const colors = require("colors");
-const moragan = require("morgan");
+const cors = require("cors");
+const morgan = require("morgan");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
-
-//dotenv conig
+const colors = require("colors");
+const connectDb = require("./config/connectDb");
+// config dot env file
 dotenv.config();
 
-//mongodb connection
-connectDB();
+//databse call
+connectDb();
 
-//rest obejct
+//rest object
 const app = express();
 
 //middlewares
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(moragan("dev"));
+app.use(cors());
 
 //routes
+app.use("/api/v1/users", require("./routes/userRoute"));
 
 //port
-const port = process.env.PORT || 8080;
-//listen port
-app.listen(port, () => {
-  console.log(
-    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
-      .bgCyan.white
-  );
+const PORT = 8080 || process.env.PORT;
+
+//listen server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
